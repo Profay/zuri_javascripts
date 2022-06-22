8c045260b506d09da50b197154594b52e3423289
profytimmy <oyewaletimmy01@yahoo.com>
2022-06-22 12:54:50 +0000

    good to go

---
+++ main.js
@@ -0,0 +1,7 @@
+let myName = "Oyewale Oluwashogo Ayomide"
+let myHeight = 70
+let myCountry = "Nigeria"
+
+alert("Name: " + myName)
+alert("Height: " + myHeight+" inch")
+alert("Country: " + myCountry)
